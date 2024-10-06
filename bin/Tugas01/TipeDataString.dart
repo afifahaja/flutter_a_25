2a0794ce97faf96b227c64e4dfba18fa4a162296
afifahaja <afifahnoor58@gmail.com>
2024-10-06 09:09:29 +0800

    TipeDataSringDanLainLain

---
+++ bin/Tugas01/TipeDataString.dart
@@ -0,0 +1,50 @@
+
+//Tipe data String
+// void main(){
+//   String firstName ="Afifah";
+
+//   String lastName = "Noor";
+
+//   print(firstName);
+//   print(lastName);
+// }
+
+//Expression
+// void main(){
+//   String firstName = "Afifah";
+//   String lastName = "Noor";
+
+//   var fullName = '$firstName ${lastName}';
+
+//   print(fullName);
+// }
+
+// //Karakter Backslash
+// void main(){
+//   var text = 'this is \'dart\' \$cool';
+
+//   print(text);
+
+// }
+
+//menggabungkan String(kd mau)
+// void main(){
+//   var firstName = 'Afifah';
+//   var lastName = 'noor';
+
+//   var name1 = firstName + lastName;
+//   var name2 = 'Afifah' 'noor' 'huang';
+
+//   print(name1);
+//   print(name2);
+// }
+
+// //Multiline String
+void main(){
+  var longString = '''
+  string ini sangat panjang sehingga sulit
+  dibuat dalam satu baris kode program''';
+
+  print(longString);
+}
+
