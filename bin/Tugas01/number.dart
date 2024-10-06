11591504f5ab64a0cf6af3657fa382466e51d4b2
afifahaja <afifahnoor58@gmail.com>
2024-10-06 08:32:14 +0800

    Number

---
+++ bin/Tugas01/number.dart
@@ -0,0 +1,19 @@
+//Tipe data number
+// void main(){
+//   int number1 = 10;
+//   double number2 = 10.5;
+
+//   print(number1);
+//   print(number2);
+
+// }
+
+//Num
+void main(){
+  num number = 10;
+  print(number);
+
+  number = 10.5;
+  print(number);
+
+}
