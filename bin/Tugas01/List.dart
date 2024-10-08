ea29c8c025e3ee6e017e0fbc82443c8c6f0b61b0
afifahaja <afifahnoor58@gmail.com>
2024-10-08 11:01:47 +0800

    List

---
+++ bin/Tugas01/List.dart
@@ -0,0 +1,46 @@
+// void main(){
+//   List<int> listInt = [];
+
+//   var listString = <String>[];
+// }
+
+//menambhakan data di list
+// void main(){
+//   var names = <String>[];
+
+//   names.add('Afifah');
+//   names.add('Noor');
+//   names.add('Huang');
+
+//   print(names);
+//   print(names.length);
+// }
+
+//Manipulasi Data diList
+// void main(){
+//   var names = <String>[];
+
+//   names.add('Afifah');
+//   names.add('Noor');
+//   names.add('Huang');
+
+//   print(names);
+//   print(names.length);
+
+//   print(names[0]);
+//   names[0] = 'Afifah';
+//   names.removeAt(2);
+//   print(names);
+
+// }
+
+//CARA MEMBUAT LIST DENGAN DEKLARASI SECARA LANGSUNG
+void main(){
+  var namaOrang = ['Afifah', 'Noor', 'Huang'];
+  var umurOrang = <int>[21, 20, 24];
+  var nilaiOrang = [90,89,100];
+
+  print(namaOrang);
+  print(umurOrang);
+  print(nilaiOrang);
+}
