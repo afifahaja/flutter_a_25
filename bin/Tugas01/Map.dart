06d059fdc18f0a89c6688361f90ea393d46a0678
afifahaja <afifahnoor58@gmail.com>
2024-10-08 11:17:18 +0800

    Map

---
+++ bin/Tugas01/Map.dart
@@ -0,0 +1,36 @@
+//Membuat map
+// void main(){
+//   Map<String, String> person ={};
+//   var product = Map<String, String>();
+//   var address = <String, String>{};
+
+//   print(person);
+//   print(product);
+//   print(address);
+// }
+
+// void main(){
+//   var name =<String,String>{};
+//   name['first'] = 'Noor';
+//   name['middle'] = 'Afifah';
+//   name['last'] = 'Huang';
+
+//   print(name['first']);
+
+//   name['middle'] = 'Afifah';
+//   print(name);
+
+//   name.remove('last');
+//   print(name);
+// }
+
+//INI ADALAH CONTOH MENDEKLARASIKAN MAP SECARA LANGSUNG
+void main(){
+  var nama = {
+    'awal':'Noor',
+    'tengah':'Afifah',
+    'akhir':'Huang'
+  };
+
+  print(nama['awal']);
+}
