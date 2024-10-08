b14f88c7019354c830956b689bf0cce7e5606a24
afifahaja <afifahnoor58@gmail.com>
2024-10-08 10:41:15 +0800

    KonversiTipeData

---
+++ bin/Tugas01/KonversiTipeData.dart
@@ -0,0 +1,34 @@
+//Konversi String dan Number
+// void main(){
+//   var inputString = '1000';
+//   var inputInt = int.parse(inputString);
+//   var inputDouble = double.parse(inputString);
+
+//   var doubleFromInt = inputInt.toDouble();
+//   var intFromDouble = inputDouble.toInt();
+
+//   var stringFromInt = inputInt.toString();
+//   var stringfromDouble = inputDouble.toString();
+
+//   print(inputDouble);
+//   print(inputInt);
+//   print(inputString);
+
+//   print(doubleFromInt);
+//   print(intFromDouble);
+
+//   print(stringfromDouble);
+//   print(stringFromInt);
+// }
+
+//Konversi Boolean dan String
+void main(){
+  var inputString = 'true';
+  var inputBool = inputString == 'true';
+
+  var StringFromBool = inputBool.toString();
+
+  print(inputString);
+  print(inputBool);
+  print(StringFromBool);
+}
