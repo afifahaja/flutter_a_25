014f46c90bf46ffd974941b2a9fe248864d12181
afifahaja <afifahnoor58@gmail.com>
2024-10-08 12:15:59 +0800

    Null ukang

--- bin/Tugas01/Null.dart
+++ bin/Tugas01/Null.dart
@@ -1,7 +1,7 @@
-void main(){
-  int number1;
-  int? number2;
+// void main(){
+//   int number1;
+//   int? number2;
 
-  print(number1); // erorr
-  print(number2); //null
-}
+//   print(number1); // erorr
+//   print(number2); //null
+// }
