b84e11aa70d456b9eabec8d83a0a8aa4475f1c27
afifahaja <afifahnoor58@gmail.com>
2024-10-09 16:20:54 +0800

    Function

---
+++ bin/Tugas01/SemuaFunction.dart
@@ -0,0 +1,18 @@
+//function
+  // void sayHello(){
+  //   print('Hallo Guys');
+  // }
+  // void main(){
+  //   sayHello();
+  //   sayHello();
+  //     }
+
+//function parameter
+void sayHello(String firstName, String lastName){
+  print('Hello $firstName $lastName');
+
+}
+void main(){
+  sayHello('Noor', 'Afifah');
+  sayHello('Huang', 'Renjun');
+}
