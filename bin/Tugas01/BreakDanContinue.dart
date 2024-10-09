131d6298205ccc8f785d203a105cab49b0c0189f
afifahaja <afifahnoor58@gmail.com>
2024-10-09 15:51:18 +0800

    Break dan continue

---
+++ bin/Tugas01/BreakDanContinue.dart
@@ -0,0 +1,23 @@
+//Break
+// void main(){
+//   var counter = 1;
+
+//   while (true){
+//     print('Perulangan ke-$counter');
+//     counter++;
+
+//     if (counter > 10){
+//       break;
+//     }
+//   }
+// }
+
+//continue
+void main(){
+  for (var counter = 1; counter <=100; counter++){
+    if (counter % 2 == 0){
+      continue;
+    }
+    print('Perulangan Ganjil-$counter');
+  }
+}
