c1a37c552a41d4586f55ad231ac8bab0315dc0dd
afifahaja <afifahnoor58@gmail.com>
2024-10-09 15:30:22 +0800

    foorloop

---
+++ bin/Tugas01/ForLoop.dart
@@ -0,0 +1,24 @@
+//Perulangan Tanpa Henti
+// void main(){
+//   for (;;){
+//     print('Perulangan Tanpa Henti');
+//   }
+// }
+
+//Perulangan Dengan Kondisi
+// void main(){
+//   var counter = 1;
+
+//   for (; counter <= 10 ;){
+//     print('Perulangan ke $counter');
+//     counter++;
+//   }
+// }
+
+//Perulangan Dengan Init Statement
+void main(){
+  for (var counter = 1; counter <= 10 ;){
+    print('Perulangan ke-$counter');
+    counter++;
+  }
+}
