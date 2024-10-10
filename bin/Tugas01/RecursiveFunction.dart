fd6f24125df22087fd899d2bc6f4d655482c26af
afifahaja <afifahnoor58@gmail.com>
2024-10-10 13:25:16 +0800

    RecutsiveFunction

--- bin/Tugas01/RecursiveFunction.dart
+++ bin/Tugas01/RecursiveFunction.dart
@@ -23,12 +23,3 @@
     print(factorialRecursive(15));
   }
 
-//   //masalah dengan recursive
-// void loop(int value){
-//   if (value == 0) {
-//     print('Selesai');
-//   } else {
-//     print('Loop-$value');
-//     loop(value - 1);
-//   }
-// }
