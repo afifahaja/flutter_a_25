02c78401b7791d85953ae2f025b180f61322f561
afifahaja <afifahnoor58@gmail.com>
2024-10-10 13:30:04 +0800

    Functionn

---
+++ bin/Tugas01/Function.dart
@@ -0,0 +1,25 @@
+//function Short Expression
+
+// int sum(int first, int second) => first + second;
+
+// void main(){
+//   print(sum(10, 10));
+//   print(sum(20, 20));
+// }
+
+//Inner Function
+// void main(){
+  
+//   void sayHello(){
+//     print('Hello Inner Function');
+//   }
+//   sayHello();
+//   sayHello();
+// }
+
+//Main Function
+
+//Main Function Parameter
+void main(List<String> args){
+  print('Argumen : $args');
+}
