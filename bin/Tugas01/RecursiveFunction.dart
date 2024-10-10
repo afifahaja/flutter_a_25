a18412178de9c58d37a245571e8b3d4155097b1c
afifahaja <afifahnoor58@gmail.com>
2024-10-10 13:15:20 +0800

    Recursive

---
+++ bin/Tugas01/RecursiveFunction.dart
@@ -0,0 +1,34 @@
+//Factorial
+
+  int factorialLoop(int value){
+    var result = 1;
+    for (var i = 1; i <= value; i++){
+      result *= i;
+    }
+     return result;
+  }
+ 
+ //fuctorial recursive
+  int factorialRecursive(int value){
+    if (value == 1) {
+      return 1;
+    } else {
+      return value * factorialRecursive(value -1);
+    }
+  }
+
+   void main(){
+    print(factorialLoop(15));
+    print(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10 * 11 * 12 * 13 * 14 * 15);
+    print(factorialRecursive(15));
+  }
+
+//   //masalah dengan recursive
+// void loop(int value){
+//   if (value == 0) {
+//     print('Selesai');
+//   } else {
+//     print('Loop-$value');
+//     loop(value - 1);
+//   }
+// }
