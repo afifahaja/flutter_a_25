713712fb75a5f67d11388dcd9937bff618d6db6a
afifahaja <afifahnoor58@gmail.com>
2024-10-21 15:23:08 +0800

    Class dan Object

---
+++ bin/Tugas02/class.dart
@@ -0,0 +1,15 @@
+//ini adalah Class
+class person {
+
+}
+
+//ini adalah object
+void main(){
+  var person1 = person();
+  print(person1);
+
+  person person2 = person();
+   print(person2);
+  
+ 
+}
