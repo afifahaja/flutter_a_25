7cf45c1e83f847d132a0d1beb96fb08ee44a0c47
afifahaja <afifahnoor58@gmail.com>
2024-10-21 19:06:50 +0800

    Factory Constructor

---
+++ bin/Tugas02/FactoryConstructor.dart
@@ -0,0 +1,16 @@
+class Database {
+  Database(){
+    print('creat new database');
+  }
+  static Database database = Database();
+
+  factory Database.get(){
+    return database;
+  }
+}
+void main(){
+  var database1 = Database.get();
+  var database2 = Database.get();
+
+  print(database1 == database2);
+}
