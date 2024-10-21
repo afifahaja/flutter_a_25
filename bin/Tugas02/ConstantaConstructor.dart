9b17a2ed078ea0b76963121980526d97e1783910
afifahaja <afifahnoor58@gmail.com>
2024-10-21 19:02:54 +0800

    Constanta constructor

---
+++ bin/Tugas02/ConstantaConstructor.dart
@@ -0,0 +1,12 @@
+class ImmutablePoint {
+  final int x;
+  final int y;
+
+  const ImmutablePoint(this.x, this.y);
+}
+void main(){
+  var point1 = const ImmutablePoint(20, 20);
+  var point2 = const ImmutablePoint(20, 20);
+
+  print(point1 == point2);
+  }
