2f76dceab600defc76fbad9859a42a3599a9c1df
afifahaja <afifahnoor58@gmail.com>
2024-10-22 11:09:26 +0800

    type check dan cast

---
+++ bin/Tugas02/TypeCheck&Casts.dart
@@ -0,0 +1,27 @@
+class Employee{
+  String name;
+  Employee(this.name);
+}
+class Manager extends Employee{
+  Manager(String name) : super(name);
+}
+class VicePresident extends Manager{
+  VicePresident(String name) : super(name);
+}
+void sayHello(Employee employee){
+  if (employee is VicePresident){
+    VicePresident vicePresident = employee as VicePresident;
+    print('Hello Vp ${vicePresident.name}');
+  }else if (employee is Manager){
+    Manager manager = employee as Manager;
+    print('Hello Manager ${manager.name}');
+  }else{
+    print('Hello ${employee.name}');
+  }
+}
+
+void main(){
+  sayHello(Employee('Afifah'));
+  sayHello(Manager('Huang'));
+  sayHello(VicePresident('Renjun'));
+}
