9b6a43ad2a72ecc5c98242397c048e73b4a0073f
afifahaja <afifahnoor58@gmail.com>
2024-10-29 10:44:52 +0800

    access modifiler

---
+++ bin/Tugas02/accessModifier.dart
@@ -0,0 +1,7 @@
+import '../data/product.dart';
+void main() {
+  var product = Product();
+  product.id = '1';
+  product.name = 'Laptop';
+  //product._quantity = 100; //Tidak bisa diakses karna filenya berbeda
+}
