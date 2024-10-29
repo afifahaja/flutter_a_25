515f83b87782cad2728aec4f6e2a17512667081a
afifahaja <afifahnoor58@gmail.com>
2024-10-29 10:45:20 +0800

    getter dan setter

---
+++ bin/Tugas02/Getter&Setter.dart
@@ -0,0 +1,9 @@
+import '../data/Rectangel.dart';
+
+void main() {
+  var rectangle = Rectangle();
+  rectangle.width = 100;
+  print(rectangle.width);
+  rectangle.length = 200;
+  print(rectangle.length);
+}
