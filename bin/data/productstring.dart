ca6c81ac2f27e5ca82e318528e2aa8236dea2a91
afifahaja <afifahnoor58@gmail.com>
2024-10-29 10:54:28 +0800

    To String Method

---
+++ bin/data/productstring.dart
@@ -0,0 +1,14 @@
+class Product {
+  String? id;
+  String? name;
+
+  int? _quantity; //tidak bisa di akses directory
+
+  int? getQuantity(){
+    return _quantity;
+
+  }
+  String toString(){
+    return "Product{id=$id, name=$name, quantity=$_quantity}";
+  }
+}
