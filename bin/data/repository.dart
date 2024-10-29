a2c8ee100fee342bd1c4d523409c7ed2e127caef
afifahaja <afifahnoor58@gmail.com>
2024-10-29 11:22:25 +0800

    No Such Method

---
+++ bin/data/repository.dart
@@ -0,0 +1,19 @@
+import 'dart:nativewrappers/_internal/vm/lib/mirrors_patch.dart';
+abstract class CategoryRepository {
+  void id(String id);
+  void name(String name);
+}
+
+//ini nosuchMethode
+class Repository extends CategoryRepository{
+  final String _name;
+  Repository(this._name);
+
+  
+  noSuchMethod(Invocation invocation){
+    var column = MirrorSystem.getName(invocation.memberName);
+    var value = invocation.positionalArguments.first;
+    var sql = "select * from $_name where $column = '$value'";
+    print(sql);
+  }
+}
