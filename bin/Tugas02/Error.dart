608a40c0bb674d1b7edb69ddf51e19214b6a63af
afifahaja <afifahnoor58@gmail.com>
2024-10-31 16:53:01 +0800

    Error

---
+++ bin/Tugas02/Error.dart
@@ -0,0 +1,15 @@
+ //Error
+// void main() {
+//   var list = ["NOOR", "AFIFAH", "HUANG"];
+//   print(list[10]);
+// }
+
+
+//Ini Tidak Error
+// void main() {
+//   var list = ["NOOR", "AFIFAH", "HUANG"];
+
+//   if (list.length > 10) {
+//     print(list[10]);
+//   }
+// }
